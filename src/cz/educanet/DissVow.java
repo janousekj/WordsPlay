f22c925538e170e94f97a4a5e475c42f8e65ff86
Tomáš Valenta <semanticer@gmail.com>
2015-09-15 21:29:38 +0200

    Added Docs
    Added documantion for DissVow class

--- src/cz/educanet/DissVow.java
+++ src/cz/educanet/DissVow.java
@@ -1,11 +1,11 @@
 package cz.educanet;
 
 /**
- * Created by semanticer on 15. 9. 2015.
+ * Helper class to carry result from disemvoweler function
  */
 public class DissVow {
 
-    public final String diss;
+    public final String diss; 
     public final String vow;
 
     public DissVow(String diss, String vow) {
